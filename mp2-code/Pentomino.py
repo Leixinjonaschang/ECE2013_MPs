a196c2704fc5edb40c284372830eeb16aac1372a
Leixinjonaschang <leixinchang666@gmail.com>
2023-10-31 04:19:09 +0800

    Upload the solve.py.

--- mp2-code/Pentomino.py
+++ mp2-code/Pentomino.py
@@ -4,43 +4,46 @@
 import numpy as np
 import instances
 from solve import solve
-    
+
+
 def get_pent_idx(pent):
     """
     Returns the index of a pentomino.
     """
-    pidx = 0
-    for i in range(pent.shape[0]):
-        for j in range(pent.shape[1]):
-            if pent[i][j] != 0:
-                pidx = pent[i][j]
-                break
-        if pidx != 0:
-            break
-    if pidx == 0:
-        return -1
-    return pidx - 1
-        
-def is_pentomino(pent, pents):
+    pidx = 0  # Index of pentomino
+    for i in range(pent.shape[0]):  # Find first non-zero element
+        for j in range(pent.shape[1]):  # Find first non-zero element
+            if pent[i][j] != 0:  # Found non-zero element
+                pidx = pent[i][j]  # Index of pentomino
+                break  # Break out of inner loop
+        if pidx != 0:  # Break out of outer loop
+            break  # Break out of outer loop
+    if pidx == 0:  # Pentomino not found
+        return -1  # Pentomino not found
+    return pidx - 1  # Pentomino found
+
+
+def is_pentomino(pent, pents):  # pent is a numpy array
     """
     Checks if a pentomino pent is part of pents
     """
-    pidx = get_pent_idx(pent)
-    if pidx == -1:
+    pidx = get_pent_idx(pent)  # Index of pentomino
+    if pidx == -1:  # Pentomino not found
         return False
-    true_pent = pents[pidx]
-    
-    for flipnum in range(3):
-        p = np.copy(pent)
-        if flipnum > 0:
-            p = np.flip(pent, flipnum-1)
-        for rot_num in range(4):
-            if np.array_equal(true_pent, p):
-                return True
-            p = np.rot90(p)
-    return False
-                        
-def add_pentomino(board, pent, coord, check_pent=False, valid_pents=None):
+    true_pent = pents[pidx]  # Pentomino found
+
+    for flipnum in range(3):  # Check all flips
+        p = np.copy(pent)  # Copy pentomino
+        if flipnum > 0:  # Flip pentomino
+            p = np.flip(pent, flipnum - 1)  # Flip pentomino
+        for rot_num in range(4):  # Check all rotations
+            if np.array_equal(true_pent, p):  # Check if pentominos are equal
+                return True  # Pentomino found
+            p = np.rot90(p)  # Rotate pentomino
+    return False  # Pentomino not found
+
+
+def add_pentomino(board, pent, coord, check_pent=False, valid_pents=None):  # pent is a numpy array
     """
     Adds a pentomino pent to the board. The pentomino will be placed such that
     coord[0] is the lowest row index of the pent and coord[1] is the lowest 
@@ -48,61 +51,65 @@ def add_pentomino(board, pent, coord, check_pent=False, valid_pents=None):
     
     check_pent will also check if the pentomino is part of the valid pentominos.
     """
-    if check_pent and not is_pentomino(pent, valid_pents):
-        return False
-    for row in range(pent.shape[0]):
-        for col in range(pent.shape[1]):
-            if pent[row][col] != 0:
-                if board[coord[0]+row][coord[1]+col] != 0: # Overlap
-                    return False
-                else:
-                    board[coord[0]+row][coord[1]+col] = pent[row][col]
-    return True
-    
+    if check_pent and not is_pentomino(pent, valid_pents):  # Check if pentomino is valid
+        return False  # Pentomino not valid
+    for row in range(pent.shape[0]):  # Check if pentomino can be placed
+        for col in range(pent.shape[1]):  # Check if pentomino can be placed
+            if pent[row][col] != 0:  # Pentomino tile
+                if board[coord[0] + row][coord[1] + col] != 0:  # Pentomino cannot be placed
+                    return False  # Pentomino cannot be placed
+                else:  # Pentomino can be placed
+                    board[coord[0] + row][coord[1] + col] = pent[row][col]  # Place pentomino
+    return True  # Pentomino can be placed
+
+
 def remove_pentomino(board, pent_idx):
-    board[board==pent_idx+1] = 0
-        
+    # Remove pentomino from board
+    board[board == pent_idx + 1] = 0  # Remove pentomino from board
+
+
 def check_correctness(sol_list, board, pents):
     """
     Sol is a list of pentominos (possibly rotated) and their upper left coordinate
     """
     # All tiles used
-    if len(sol_list) != len(pents):
+    if len(sol_list) != len(pents):  # Check if all tiles used
         return False
     # Construct board
-    sol_board = np.zeros(board.shape)
-    seen_pents = [0]*len(pents)
-    for pent, coord in sol_list:
-        pidx = get_pent_idx(pent)
-        if seen_pents[pidx] != 0:
+    sol_board = np.zeros(board.shape)  # Construct board
+    seen_pents = [0] * len(pents)  # Construct seen_pents
+    for pent, coord in sol_list:  # Check if pentomino can be placed
+        pidx = get_pent_idx(pent)  # Index of pentomino
+        if seen_pents[pidx] != 0:  # Check if pentomino has been seen
+            return False  # Pentomino has been seen
+        else:  # Pentomino has not been seen
+            seen_pents[pidx] = 1  # Pentomino has been seen
+        if not add_pentomino(sol_board, pent, coord, True, pents):  # Check if pentomino can be placed
             return False
-        else:
-            seen_pents[pidx] = 1
-        if not add_pentomino(sol_board, pent, coord, True, pents): 
-            return False
-            
+
     # Check same number of squares occupied
-    if np.count_nonzero(board) != np.count_nonzero(sol_board):
+    if np.count_nonzero(board) != np.count_nonzero(sol_board):  # Check if same number of squares occupied
         return False
     # Check overlap
-    if np.count_nonzero(board) != np.count_nonzero(np.multiply(board, sol_board)):
+    if np.count_nonzero(board) != np.count_nonzero(np.multiply(board, sol_board)):  # Check if overlap
         return False
-    
+
     return True
-        
+
 
 if __name__ == "__main__":
     """
     Run python Pentomino.py to check your solution. You can replace 'board' and 
     'pents' with boards of your own. You can start off easy with simple dominos.
     
-    We won't gaurantee which tests your code will be run on, however if it runs
+    We won't guarantee which tests your code will be run on, however if it runs
     well on the pentomino set you should be fine. 
     """
     board = instances.board_6x10
     pents = instances.dominos
-    sol_list = solve(board, pents)
-    if check_correctness(sol_list, board, pents):
-        print("PASSED!")
+    sol_list = solve(board,
+                     pents)  # sol_list is a list of pentominos (possibly rotated) and their upper left coordinate
+    if check_correctness(sol_list, board, pents):  # Check if solution is correct
+        print("PASSED!")  # Solution is correct
     else:
-        print("FAILED...")
+        print("FAILED...")  # Solution is incorrect
--- mp2-code/solve.py
+++ mp2-code/solve.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import time
 
 
 def solve(board, pents):
@@ -6,13 +10,162 @@ def solve(board, pents):
     This is the function you will implement. It will take in a numpy array of the board
     as well as a list of n tiles in the form of numpy arrays. The solution returned
     is of the form [(p1, (row1, col1))...(pn,  (rown, coln))]
-    where pi is a tile (may be rotated or flipped), and (rowi, coli) is 
-    the coordinate of the upper left corner of pi in the board (lowest row and column index 
+    where pi is a tile (may be rotated or flipped), and (rowi, coli) is
+    the coordinate of the upper left corner of pi in the board (lowest row and column index
     that the tile covers).
-    
+
     -Use np.flip and np.rot90 to manipulate pentominos.
-    
+
     -You may assume there will always be a solution.
     """
-    
-    raise NotImplementedError
+    board = np.array(board)
+
+    # change the board a litte to be compatible with the pents
+    board[board == 0] = -1
+    board[board == 1] = 0
+
+    pents_dict = {}
+    for p in pents:
+        pents_dict[label_pent(p)] = get_variants(p)
+
+    start_time = time.time()
+    # get the result board first
+    _, res_board = recursive_dfs(board, dict(pents_dict))
+    end_time = time.time()
+    print(f"The result board with pent labels is: \n {res_board}")
+    print(f"The time used to find the result board with recursive DFS is: {end_time - start_time}")
+
+    visualize_result_board(res_board)  # visualize the result board
+
+    # produce the final solution
+    pents_coords = defaultdict(list)
+    for x, y in np.ndindex(res_board.shape):
+        if res_board[x][y] in pents_dict:
+            pents_coords[res_board[x][y]].append((x, y))
+
+    solution = []
+
+    for plabel, coords in pents_coords.items():
+        min_x, max_x, min_y, max_y = coords[0][0], coords[0][0], coords[0][1], coords[0][1]
+        for coord in coords:
+            min_x, max_x = min(min_x, coord[0]), max(max_x, coord[0])
+            min_y, max_y = min(min_y, coord[1]), max(max_y, coord[1])
+        # create a piece with the minimum size for the pent
+        piece = np.zeros((max_x - min_x + 1, max_y - min_y + 1))
+        for coord in coords:
+            piece[coord[0] - min_x][coord[1] - min_y] = plabel
+        solution.append((piece.astype(int), (min_x, min_y)))
+        
+    return solution
+
+
+def recursive_dfs(board, pents_dict):
+    # recursively call the function to find the solution with depth first search
+    if not pents_dict:  # if all the pents are placed, return the result
+        return True, board
+
+    board_shape = board.shape
+
+    for x, y in ((x, y) for y in range(board_shape[1]) for x in range(board_shape[0])):
+        # find the the first unfilled position to add a pent
+        if board[x][y] == 0:
+            for p, variants in pents_dict.items():
+                # try all the variants of a pent
+                for variant in variants:
+                    to_continue = False
+
+                    # check if it's possible to add the pent on the board
+                    for x_move, y_move in variant:
+                        if not in_bound_test(board_shape, x + x_move, y + y_move) or board[x + x_move][y + y_move] != 0:
+                            # if the pent is out of bound or overlaps with other pents, try the next variant
+                            to_continue = True
+                            break  # break the loop if we cannot add a new pent
+                    # continue to the next loop if we cannot add a new pent
+                    if to_continue:
+                        continue
+
+                    # create a new board and a new pents_map for recursion
+                    new_board = np.array(board)
+
+                    new_pents_map = dict(pents_dict)
+                    new_pents_map.pop(p)
+
+                    # add the pent to the board
+                    for x_move, y_move in variant:
+                        new_board[x + x_move][y + y_move] = p
+
+                    # recursively call the function to find the solution
+                    found, res_board = recursive_dfs(new_board, new_pents_map)
+
+                    if found:
+                        return True, res_board
+
+            return False, board
+
+
+def in_bound_test(board_shape, x, y):  # check if a coordinate is in bound
+    return 0 <= x < board_shape[0] and 0 <= y < board_shape[1]
+
+
+def get_variants(pentomino):
+    # get all the variants of a pentomino, like flipping and rotating
+    variants = set()  # use a set to remove duplicates
+    for flip_count in range(3):
+        flipped_pentomino = np.copy(pentomino)
+        if flip_count > 0:
+            flipped_pentomino = np.flip(pentomino, flip_count - 1)
+        for rotation_count in range(4):
+            rotated_pentomino = np.rot90(flipped_pentomino, rotation_count)
+            variants.add(reformulate_variant(rotated_pentomino))
+
+    return list(variants)
+
+
+def reformulate_variant(variant):
+    # reformulate the variant to a tuple of coordinates
+    coords = []
+    for x, y in np.ndindex(variant.shape):
+        if variant[x][y] != 0:
+            coords.append((x, y))
+
+    origin = coords[0]
+
+    for coord in coords:
+        if coord[1] < origin[1]:
+            origin = coord
+        elif coord[1] == origin[1] and coord[0] < origin[0]:
+            origin = coord
+
+    variant = tuple()
+    for coord in coords:
+        variant += ((coord[0] - origin[0], coord[1] - origin[1]),)
+
+    return tuple(sorted(variant))
+
+
+def label_pent(pent):
+    # label the pentomino
+    for i in range(pent.shape[0]):
+        for j in range(pent.shape[1]):
+            if pent[i][j] != 0:
+                plabel = pent[i][j]
+                break
+        if plabel != 0:
+            break
+    return plabel
+
+
+def visualize_result_board(res_board):
+    # color the board for better visualization
+    color_board = np.zeros(res_board.shape)
+    max_element = np.max(res_board)
+    for x, y in np.ndindex(res_board.shape):
+        if res_board[x][y] != 0:
+            color_board[x][y] = res_board[x][y] / (max_element + 1)
+
+    # plot the colored board for 2 seconds and close the plot
+    plt.imshow(color_board, cmap='viridis', interpolation='nearest')
+    plt.show(block=False)  # set block=False to make the plot not block the code to proceed
+    plt.title("The visualization of the result board")
+    plt.pause(3)
+    plt.close()
